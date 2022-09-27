fcee4b7135a55555e917474e007fe26e9813b34c
rdokamoto@gmail.com <ryan.okamoto@Ryans-MacBook-Pro.local>
2022-09-27 09:58:49 -0700

    first commit, functionality completed

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { getUser } from "../../utilities/users-service";
+import AuthPage from "../AuthPage/AuthPage";
+import NavBar from "../../components/NavBar/NavBar";
+import Calculator from "../../components/Calculator/Calculator"
+
+export default function App() {
+  const [user, setUser] = useState(getUser());
+
+  return (
+    <main className="App">
+      <Calculator />
+
+      {/* {user ? (
+        <>
+          <NavBar user={user} setUser={setUser} />
+          <Routes></Routes>
+        </>
+      ) : (
+        <AuthPage setUser={setUser} />
+      )} */}
+    </main>
+  );
+}
